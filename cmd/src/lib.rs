fc938c3b8f55b50140e1ec3b47b016079c3e17bf
May <MayflowerC10@gmail.com>
2025-09-11 04:09:30 -0400

    handle trailing args

--- cmd/src/lib.rs
+++ cmd/src/lib.rs
@@ -4,6 +4,8 @@ use clap::Parser;
 #[derive(Parser)]
 pub struct Cli {
     pub path: Option<PathBuf>,
+    #[arg(trailing_var_arg = true)]
+    pub args: Vec<String>
 }
 
 impl Cli {
@@ -12,5 +14,17 @@ impl Cli {
         if let Some(path) = &self.path {
             read_to_string(path).ok()
         } else { None }
+    }
+
+    pub fn args(&self) -> Vec<(i64, String)> {
+        let mut idx = 1;
+        let mut v = self.args.iter().map(|s| {
+            idx += 1;
+            (idx - 1, s.to_string())
+        }).collect::<Vec<_>>();
+        if let Some(p) = self.path.as_ref() {
+            v.push((0, p.to_str().unwrap().to_string()));
+        }
+        v
     }
 }
